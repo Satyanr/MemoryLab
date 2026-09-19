1446f552bf3b2c8717f2764e9f391b90717b3bb0
Satyanr <satyanr84@gmail.com>
2026-09-19 15:02:22 +0700

    Add MemoryLab.TestTarget assembly and project files for .NET 8.0
    - Created MemoryLab.TestTarget.dll and MemoryLab.TestTarget.csproj files.
    - Added project.assets.json, project.nuget.cache, and related NuGet configuration files.
    - Configured project to target .NET 8.0 with necessary dependencies.
    - Included assembly metadata and trust information in the assembly manifest.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Windows;
--- Services/RecentProjectService.cs
+++ Services/RecentProjectService.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Text.Json;
 
 namespace MemoryLab.Services;
